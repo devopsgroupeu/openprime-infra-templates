952866794da53f3fcd9b3148f91df92b5642a71f
Adam Virostek <avirostek@devopsgroup.sk>
2026-03-06 12:33:44 +0100

    fix: set load_config_file to false in kubectl provider configuration

--- templates/terraform/kubernetes/_config.tf
+++ templates/terraform/kubernetes/_config.tf
@@ -64,6 +64,7 @@ provider "kubectl" {
   host                   = data.terraform_remote_state.aws.outputs.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(data.terraform_remote_state.aws.outputs.eks_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.eks.token
+  load_config_file       = false
 }
 
 data "aws_eks_cluster_auth" "eks" {
